1bd661ddc7d773803d40fd9dec82ad86386da5dc
cactusaurelius <39528088+cactusaurelius@users.noreply.github.com>
2021-03-01 16:53:54 +0300

    cln

--- commands/health.js
+++ commands/health.js
@@ -1,21 +1,8 @@
 module.exports = {
-  command: 'health',
-  description: 'Ping Registered nodes',
-  //   alias: 'hi',
-  // options: [
-  //     { option: "-u, --uppercase", description: "Uppercase the name" }
-  // ],
-  // types: {
-  //     string: ["name"],
-  //     boolean: ["u", "uppercase"]
-  // },
-  //parse(command, args) {},
-  //validate(args) {},
-  //help(args) {},
-  //   allowUnknownOptions: true,
-  action(broker /*, args , helpers*/) {
-    // const name = args.options.uppercase ? args.name.toUpperCase() : args.name;
-    return broker.ping().then(nodes => {
+  command: "health",
+  description: "Ping Registered nodes",
+  action(broker) {
+    return broker.ping().then((nodes) => {
       for (const [nodeID, ping] of Object.entries(nodes)) {
         (!ping && broker.logger.error(`Node: ${nodeID} Is not responding`)) ||
           broker.logger.info(
